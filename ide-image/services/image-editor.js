d8fd53a1d6a33effda429e86f2653790e807bf54
StanZGenchev <stanislav.genchev@sap.com>
2022-07-05 09:41:44 +0300

    Comment update

--- ide-image/services/image-editor.js
+++ ide-image/services/image-editor.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2010-2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
